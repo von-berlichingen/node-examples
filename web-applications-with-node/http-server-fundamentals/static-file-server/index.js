011dc694264082d11dcf255b06accd3f5641885f
von-berlichingen <anis.b.amara@gmail.com>
2017-06-10 21:32:19 +0100

    handling error in static file server

--- web-applications-with-node/http-server-fundamentals/static-file-server/index.js
+++ web-applications-with-node/http-server-fundamentals/static-file-server/index.js
@@ -11,8 +11,25 @@ http
   .createServer((req, res) => {
     let url = parse(req.url);
     let path = join(root, url.pathname);
-    let stream = fs.createReadStream(path);
 
-    stream.pipe(res);
+    fs.stat(path, (err, stat) => {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('Not found');
+        } else {
+          res.statusCode = 500;
+          res.end('Internal Server Error');
+        }
+      } else {
+        let stream = fs.createReadStream(path);
+
+        stream.pipe(res);
+        stream.on('error', (err) => {
+          res.statusCode = 500;
+          res.end('Internal Server Error');
+        });
+      }
+    });
   })
   .listen(3000);
